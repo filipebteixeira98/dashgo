63d3473dae0663155a6bfc2151171869e8917f29
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-03-01 11:42:29 -0300

    chore: created sidebar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,21 @@
+import { Flex } from '@chakra-ui/react'
+
 import { Header } from '../components/Form/Header'
+import { Sidebar } from '../components/Form/Sidebar'
 
 export default function Dashboard() {
   return (
-    <Header />
+    <Flex direction="column" h="100vh">
+      <Header />
+      <Flex
+        maxWidth={1480}
+        w="100%"
+        my="6"
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
